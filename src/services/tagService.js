5b3e475bc547d5d78713f52da9869e787dbc9340
Nguyễn Thành Trung <b28082002@gmail.com>
2024-06-11 12:12:21 +0700

    test Tags fire base

--- src/services/tagService.js
+++ src/services/tagService.js
@@ -1,6 +1,12 @@
-import { get } from "../utils/request";
+// import { get } from "../utils/request";
+import { Getdb } from "../utils/requestfirebase";
 
 export const getListTag = async () => {
-  const result = await get(`tags`);
-  return result;
+  try {
+    const data = await Getdb('tags');
+    return data;
+    // Sử dụng data ở đây
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
---
+++ src/utils/requestfirebase.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+import { getDatabase, ref, set, onValue, child, get } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyB88BeRVrPXOopcvNfeBjB17BsHdQzYe58",
+    authDomain: "cdtn-3082b.firebaseapp.com",
+    databaseURL: "https://cdtn-3082b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cdtn-3082b",
+    storageBucket: "cdtn-3082b.appspot.com",
+    messagingSenderId: "630084049894",
+    appId: "1:630084049894:web:d64dbd1fff15e1e0c3d427",
+    measurementId: "G-PWTVLTH38W"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+export const fetchData = (db, path) => {
+    return new Promise((resolve, reject) => {
+        onValue(ref(db, path), (snapshot) => {
+            // console.log(path);
+            const result = snapshot.val();
+            // console.log(snapshot.val());
+            if (result !== null) {
+                resolve(result);
+            } else {
+                reject(new Error("No data found"));
+            }
+        }, (error) => {
+            reject(error);
+        });
+    });
+}
+export const Getdb = async (path) => {
+    try {
+        const resultNew = await fetchData(db, path);
+        // console.log('Updated resultNew:', resultNew);
+        return resultNew;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Đảm bảo lỗi được xử lý bên ngoài
+    }
+}
